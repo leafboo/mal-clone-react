09a8b7199a70fcad25022448203586e7ef080b93
Jan romel <janjancaranguian@gmail.com>
2024-01-08 11:57:59 +0800

    Header component finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,14 @@ import HeaderCSS from '../styles/Header.module.css'
 export default function Header() {
   return (
     <div className={HeaderCSS['header-container']}>
-      <img src="https://cdn.myanimelist.net/images/mal-logo-xsmall-white@2x.png?v=1634263200" alt="My anime list logo." />
-      <button className={HeaderCSS['login-button']}>Login</button>
-      <button className={HeaderCSS['signup-button']}>Sign Up</button>
+      <div className={HeaderCSS['image-container']}>
+        <img src="https://cdn.myanimelist.net/images/mal-logo-xsmall-white@2x.png?v=1634263200" alt="My anime list logo." />
+      </div>
+      <div className={HeaderCSS['buttons-container']}>
+        <button className={HeaderCSS['hide-ads-button']}>Hide Ads</button>
+        <button className={HeaderCSS['login-button']}>Login</button>
+        <button className={HeaderCSS['signup-button']}>Sign Up</button>
+      </div>
     </div>
   )
  
